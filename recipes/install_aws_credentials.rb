a2e29db02c24b9fd1b9ecb434c247835739ca52e
Phil Dier <phil@raventools.com>
2016-01-12 17:39:51 -0600

    fixie

--- recipes/install_aws_credentials.rb
+++ recipes/install_aws_credentials.rb
@@ -1,7 +1,7 @@
 directory "/root/.aws"
 
 file "/root/.aws/credentials" do
-	contents <<-EOH
+	content <<-EOH
 [default]
 aws_access_key_id = #{node[:raven_deploy][:aws_key]}
 aws_secret_access_key = #{node[:raven_deploy][:aws_secret]}
@@ -9,7 +9,7 @@ aws_secret_access_key = #{node[:raven_deploy][:aws_secret]}
 end
 
 file "/root/.aws/config" do
-	contents <<-EOH
+	content <<-EOH
 [default]
 region = #{node[:raven_deploy][:aws_region]}
 output = json
