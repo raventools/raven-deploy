363e913112c4aa37a9d731ec83acef958e8fa90e
Phil Dier <phil@raventools.com>
2016-02-03 11:00:56 -0600

    docroot should be nil

--- resources/app.rb
+++ resources/app.rb
@@ -7,6 +7,6 @@ attribute :owner, :kind_of => String
 attribute :repository, :kind_of => String
 attribute :revision, :kind_of => String, :default => "master"
 
-attribute :docroot, :kind_of => String, :default => "public"
+attribute :docroot, :kind_of => String
 attribute :domains, :kind_of => Array, :default => ["localhost"]
 attribute :port, :kind_of => Integer, :default => 80
