f524aa15c964b877c55dfc5aa65f00fa84529070
Phil Dier <phil@raventools.com>
2016-02-16 09:49:46 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'phil@raventools.com'
 license          'MIT'
 description      'Installs/Configures raven-deploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "aws"
 depends "apache2"
