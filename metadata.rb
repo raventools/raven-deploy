840f89b602b09a583c21b33688d6dd3878046d29
Phil Dier <phil@raventools.com>
2016-02-19 12:03:47 -0600

    recipe to fetch data bags at compile time

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      'Installs/Configures raven-deploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
+depends "build-essential"
 depends "aws"
 depends "apache2"
 depends "raven-dev"
---
+++ recipes/fetch_data_bags.rb
@@ -0,0 +1,50 @@
+chef_gem "aws-sdk" do
+	compile_time true
+end
+
+require 'aws-sdk'
+
+begin
+	# try w/o creds first, assuming instance role on ec2
+	s3 = ::Aws::S3::Client.new(
+		:region => "us-east-1"
+	)
+rescue
+	# fall back to using provided creds (vagrant)
+	s3 = ::Aws::S3::Client.new(
+		:region => "us-east-1",
+		:credentials => ::Aws::Credentials.new(
+			node[:raven_deploy][:aws_key],
+			node[:raven_deploy][:aws_secret]
+		)
+	)
+end
+
+secret_path = "/etc/chef/encrypted_data_bag_secret"
+if not ::File.exists?(secret_path)
+	s3.get_object(
+		response_target: secret_path,
+		bucket: node[:raven_deploy][:keys_bucket],
+		key: ".chef/encrypted_data_bag_secret"
+	)
+end
+
+data_bags_path = "/var/chef"
+s3.list_objects(
+	bucket: node[:raven_deploy][:keys_bucket],
+	prefix: "data_bags/"
+).contents.each do |o|
+
+	target_path = "#{data_bags_path}/#{o.key}"
+	obj_dir = ::File.dirname(target_path)
+
+	if not ::File.exists?(obj_dir) 
+		::Dir.mkdir(obj_dir)
+	end
+
+	s3.get_object(
+		key: o.key,
+		bucket: node[:raven_deploy][:keys_bucket],
+		response_target: target_path
+	)
+end
