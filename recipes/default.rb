acd85e07dc8c201304abf5d58c9075b5e31211c9
Phil Dier <phil@raventools.com>
2016-02-22 10:00:33 -0600

    reenable data bags

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 directory node[:raven_deploy][:keys_dir]
 directory node[:raven_deploy][:attachments_dir]
 
-#include_recipe "raven-deploy::fetch_data_bags"
+include_recipe "raven-deploy::fetch_data_bags"
 include_recipe "apache2"
 include_recipe "aws"
 include_recipe "raven-deploy::install_keys"
