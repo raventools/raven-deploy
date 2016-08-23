c22cb35a8daa6e8ad47230039fdc3b635e938ed0
Byron Albert <byron@raventools.com>
2016-08-23 15:24:59 -0500

    add jenkins if exists and fix perms

--- recipes/install_keys.rb
+++ recipes/install_keys.rb
@@ -1,4 +1,22 @@
-raven_deploy_tarball "code-deploy-keys.tar" do
-	bucket node[:raven_deploy][:keys_bucket]
-	directory "/root/.ssh"
+
+myusers = { "root" => "/root" }
+
+if system("getent passwd jenkins")
+	myusers["jenkins"] = Dir.home("jenkins")
+end
+
+myusers.each do |username, sshdir|
+
+	raven_deploy_tarball "code-deploy-keys.tar" do
+		bucket node[:raven_deploy][:keys_bucket]
+		directory "#{sshdir}/.ssh"
+	end
+
+	bash "fix-ssh-permisions" do
+		code <<-EOH
+			chown -R #{username} #{sshdir}/.ssh
+			chmod 0700 #{sshdir}/.ssh
+			chmod 0600 #{sshdir}/.ssh/*
+		EOH
+	end
 end
