bacd0d519017c538e03e53f699a94e3b2c95507b
Hakim Cira <hakim.cira@tapclicks.com>
2021-11-12 00:00:49 -0500

    Desactivate aws recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@ directory node[:raven_deploy][:attachments_dir]
 
 include_recipe "raven-deploy::fetch_data_bags"
 include_recipe "apache2"
-include_recipe "aws"
+# include_recipe "aws"
 include_recipe "raven-deploy::install_keys"
 include_recipe "raven-dev::gcc"
 
